f1f63b1a77cce0e4b8850866591ea06135e5d51c
Axel <dsg4857@gmail.com>
2026-02-19 17:47:57 -0500

    add: Crear paginas de Contacto y Crear Propiedad

---
+++ app/contact/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function ContactPage() {
+    return(
+        <div>
+            <h1 className='text-2xl font-bold text-center mt-10'>Contact Page</h1>
+        </div>
+    )
+}   
