7a4a79050ce9f52e140dc5c1fc147e34c2f9485d
Axel <dsg4857@gmail.com>
2026-02-22 01:57:00 -0500

    add: Añadir toaster en el layout y remover pt-20

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,11 +1,13 @@
 import { Navbar } from '@/shared/components/ui/Navbar'
 import React from 'react'
+import { Toaster } from 'sonner'
 
 export default function AuthLayout ({ children }: { children: React.ReactNode }) {
     return(
         <div className="flex flex-col min-h-screen ">
-              <main className="flex-1 pt-20">
+              <main className="flex-1">
                 {children}
+                <Toaster/>
               </main>
         </div>
     )
