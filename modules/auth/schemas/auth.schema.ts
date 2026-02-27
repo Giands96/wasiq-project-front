7bf611d04c559c888b428fdfd75b9339be2ff738
Axel <dsg4857@gmail.com>
2026-02-26 23:35:32 -0500

    add: Añadir Validaciones para función register

--- modules/auth/schemas/auth.schema.ts
+++ modules/auth/schemas/auth.schema.ts
@@ -1,10 +1,23 @@
 import { z } from 'zod';
 
 export const loginSchema = z.object({
-    email: z.email({error: (issue) => issue.input === undefined? 'Email is required' : 'Invalid email address' }),
+    email: z.email({error: (issue) => issue.input === undefined? 'El email es requerido' : 'Invalid email address' }),
     password: z.string(
         {error: (issue) => issue.input === undefined? 'Password is required' : 'Invalid password' })
         .min(6, { message: 'Password must be at least 6 characters long' })
 })
+
+export const registerSchema = z.object({
+    firstName: z.string({error: (issue) => issue.input === undefined? 'Nombre requerido' : 'Nombre inválido' }),
+    lastName: z.string({error: (issue) => issue.input === undefined? 'Apellido requerido' : 'Apellido inválido' }),
+    email: z.email({error: (issue) => issue.input === undefined? 'El email es requerido' : 'Email inválido' }),
+    password: z.string(
+        {error: (issue) => issue.input === undefined? 'Es necesario una contraseña' : 'Contraseña inválida' })
+        .min(6, { message: 'La contraseña debe de tener al menos 6 caracteres' }),
+    phoneNumber: z.string({error: (issue) => issue.input === undefined? 'Número telefónico es requerido' : 'Número telefónico inválido' })
+    
+})
+
+export type RegisterFormValues = z.infer<typeof registerSchema>;
 
 export type LoginFormValues = z.infer<typeof loginSchema>;
