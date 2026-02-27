a2d118124f40afdda3139e31cd89520d8d0af07f
Axel <dsg4857@gmail.com>
2026-02-27 13:37:38 -0500

    fix: mejorar manejo de errores en interceptores de respuesta

--- lib/axios.ts
+++ lib/axios.ts
@@ -1,5 +1,6 @@
 import axios from 'axios';
 import { useAuthStore } from '@/store/useAuthStore';
+import { ROUTES } from '@/shared/constants/routes';
 
 
 const api = axios.create({
@@ -21,6 +22,21 @@ api.interceptors.request.use(
     return Promise.reject(error);
   }
 );
+
+api.interceptors.response.use(
+  (response) => { return response; },
+  (error) => {
+    if (error.response && error.response.status === 401 || error.response.status === 403) {
+      useAuthStore.getState().logout();
+
+      if(typeof window !== 'undefined') {
+        window.location.href = ROUTES.HOME;
+      }
+
+    }
+    return Promise.reject(error);
+  }
+)
 
 
 export default api;
