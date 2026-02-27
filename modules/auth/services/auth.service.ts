60c0a6121d0a5e89b42335d130a7f2c2b1423657
Axel <dsg4857@gmail.com>
2026-02-26 23:35:14 -0500

    fix: corregir credenciales pasadas en metodo register

--- modules/auth/services/auth.service.ts
+++ modules/auth/services/auth.service.ts
@@ -1,13 +1,13 @@
 import api from '@/lib/axios';
 import {API_ENDPOINTS} from '@/shared/constants/routes';
-import { AuthResponse, LoginRequest } from '../types/auth.types';
+import { AuthResponse, LoginRequest, RegisterRequest } from '../types/auth.types';
 
 export const authService = {
     login: async (credentials: LoginRequest): Promise<AuthResponse> => {
         const {data} = await api.post<AuthResponse>(API_ENDPOINTS.AUTH.LOGIN, credentials);
         return data;
     },
-    register: async (credentials: LoginRequest): Promise<AuthResponse> => {
+    register: async (credentials: RegisterRequest): Promise<AuthResponse> => {
         const {data} = await api.post<AuthResponse>(API_ENDPOINTS.AUTH.REGISTER, credentials);
         return data;
     }
