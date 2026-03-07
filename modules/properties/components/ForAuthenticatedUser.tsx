fa085fbb4068208a27208cf5e929a32db6c02bd5
Axel <dsg4857@gmail.com>
2026-03-07 12:08:20 -0500

    refactor: Añadir visualización de correo electrónico y mejorar la estructura del componente ForAuthenticatedUser

--- modules/properties/components/ForAuthenticatedUser.tsx
+++ modules/properties/components/ForAuthenticatedUser.tsx
@@ -2,51 +2,75 @@
 
 import React from 'react';
 import { useAuthStore } from '@/store/useAuthStore';
-import { Phone } from 'lucide-react';
+import { Mail, Phone } from 'lucide-react';
 
 interface ForAuthenticatedUserProps {
     phoneNumber: string;
+    ownerEmail: string;
 }
 
-export const ForAuthenticatedUser: React.FC<ForAuthenticatedUserProps> = ({ phoneNumber }) => {
+export const ForAuthenticatedUser: React.FC<ForAuthenticatedUserProps> = ({ phoneNumber, ownerEmail } ) => {
 
     const isAuthenticated = useAuthStore(state => state.isAuthenticated);
-
-    console.log('Is authenticated:', isAuthenticated);
-    console.log('Phone number:', phoneNumber);
-
-    return(
-        <>
-            {isAuthenticated ? (
-                <div className='space-y-3'>
-                                    <div className='flex items-center gap-3'>
-                                        <div className='p-2 bg-gray-100 rounded-lg'>
-                                            <Phone className='w-4 h-4 text-gray-600' />
-                                        </div>
-                                        <div>
-                                            <p className='text-sm text-gray-500'>Teléfono</p>
-                                            <a href={`tel:${phoneNumber}`} className='font-medium '>
-                                                {phoneNumber}
-                                            </a>
-                                        </div>
-                                    </div>
+    return (
+      <>
+        {isAuthenticated ? (
+          <div className="space-y-3 text-gray-900">
+            <div className="flex-col items-center space-y-3 lg:flex-row">
+                <div className="flex items-center gap-3">
+                <div className="p-2 bg-gray-100 rounded-lg">
+                  <Mail className="w-4 h-4 text-gray-600" />
                 </div>
-            ) : (
-                <div className=' text-neutral-700'>
-                    <div className='flex items-center gap-3'>
-                        <div className='p-2 bg-gray-100 rounded-lg'>
-                                <Phone className='w-4 h-4 text-gray-600' />
-                            </div>
-                            <div>
-                                 <p className='text-sm text-gray-500'>Teléfono</p>
-                                <span className='text-gray-400'>
-                                    Inicia sesión para ver el número de contacto
-                                </span>
-                        </div>
-                    </div>
+                <div>
+                  <p className="text-sm text-gray-500">Email</p>
+                    <p className="font-medium text-gray-900">
+                      {ownerEmail}
+                    </p>
                 </div>
-            )}
-        </>
+              </div>
+              <div className="flex items-center gap-3">
+                <div className="p-2 bg-gray-100 rounded-lg">
+                  <Phone className="w-4 h-4 text-gray-600" />
+                </div>
+                <div>
+                  <p className="text-sm text-gray-500">Teléfono</p>
+                    <p className="font-medium text-gray-900">
+                      {phoneNumber}
+                    </p>
+                </div>
+              </div>
+              
+              
+              
+            </div>
+          </div>
+        ) : (
+          <div className=" text-neutral-700 flex-col space-y-4 rounded-lg border border-gray-200 bg-gray-50 p-6">
+            <div className="flex items-center gap-3">
+                <div className="p-2 bg-gray-100 rounded-lg">
+                  <Mail className="w-4 h-4 text-gray-600" />
+                </div>
+                <div>
+                  <p className="text-sm text-gray-500">Email</p>
+                <span className="text-gray-400">
+                  Inicia sesión para ver el correo electrónico
+                </span>
+                </div>
+              </div>
+            <div className="flex items-center gap-3">
+              <div className="p-2 bg-gray-100 rounded-lg">
+                <Phone className="w-4 h-4 text-gray-600" />
+              </div>
+              <div>
+                <p className="text-sm text-gray-500">Teléfono</p>
+                <span className="text-gray-400">
+                  Inicia sesión para ver el número de contacto
+                </span>
+              </div>
+            </div>
+          </div>
+        )}
+      </>
     );
 }
 
