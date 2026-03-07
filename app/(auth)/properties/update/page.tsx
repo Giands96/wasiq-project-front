0af8b5e6e3b76ad37e59ed0c9d01979b16513a1a
Axel <dsg4857@gmail.com>
2026-03-07 11:23:35 -0500

    refactor: Reemplazar contenido de la página con el componente CreatePropertyForm

--- app/(auth)/properties/update/page.tsx
+++ app/(auth)/properties/update/page.tsx
@@ -1,7 +1,10 @@
+import { CreatePropertyForm } from '@/modules/properties/components/CreatePropertyForm'
 import React from 'react'
 
 export const page = () => {
   return (
-    <div>page</div>
+    <div className='min-h-screen p-4'>
+      <CreatePropertyForm/>
+    </div>
   )
 }
