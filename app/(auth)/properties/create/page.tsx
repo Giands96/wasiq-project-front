78e7b0f345bc69da89bebe9ff6f66a2c7239abc1
Axel <dsg4857@gmail.com>
2026-03-08 17:47:14 -0500

    refactor: Añadir fondo degradado a la página de creación de propiedades

--- app/(auth)/properties/create/page.tsx
+++ app/(auth)/properties/create/page.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { CreatePropertyForm } from '@/modules/properties/components/CreatePropertyForm';
 export default function CreatePropertyPage() {
   return (
-    <div className="min-h-screen p-4">
+    <div className="min-h-screen p-4 bg-linear-to-tr from-beige to-orange-500/4">
       <CreatePropertyForm />
       
     </div>
