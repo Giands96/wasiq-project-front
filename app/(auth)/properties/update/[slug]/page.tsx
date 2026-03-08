c58ade839fd057243ef54a8b7276b74b9c432978
Axel <dsg4857@gmail.com>
2026-03-08 17:47:43 -0500

    feat: Crear página de actualización de propiedades con formulario correspondiente

---
+++ app/(auth)/properties/update/[slug]/page.tsx
@@ -0,0 +1,13 @@
+import { UpdatePropertyForm } from '@/modules/properties/components/UpdatePropertyForm'
+import React from 'react'
+
+export function UpdatePage() {
+  return (
+    <div className='min-h-screen p-4 bg-linear-to-tr from-beige to-orange-500/4'>
+      <UpdatePropertyForm/>
+    </div>
+  )
+}
+
+export default UpdatePage;
+
