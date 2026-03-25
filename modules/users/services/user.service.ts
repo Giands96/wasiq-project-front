8526ffbabb1ddc1c302445b72c6be46b56e39723
Axel <dsg4857@gmail.com>
2026-03-24 22:01:38 -0500

    feat: reorganizar importaciones y agregar métodos para obtener usuarios paginados y filtrados por rol

--- modules/users/services/user.service.ts
+++ modules/users/services/user.service.ts
@@ -1,7 +1,10 @@
-import { UpdateProfileFormValues, updateProfileSchema } from '@/modules/users/schemas/userSchemas';
 import api from '@/lib/axios';
-import { AuthResponse } from '@/modules/auth/types/auth.types';
 import { API_ENDPOINTS } from '@/shared/constants/routes';
+import { User, Role } from '@/modules/auth/types/auth.types';
+import { PaginatedResponse } from '@/modules/properties/types/property.types';
+import { UpdateProfileFormValues } from '@/modules/users/schemas/userSchemas';
+import { AuthResponse } from '@/modules/auth/types/auth.types';
+import { UserPaginationParams } from '../types/user.types';
 
 
 export const userService = {
@@ -9,5 +12,23 @@ export const userService = {
     updateProfile: async (userId: number, data: UpdateProfileFormValues): Promise<AuthResponse> => {
         const response = await api.post<AuthResponse>(API_ENDPOINTS.AUTH.UPDATE_PROFILE(userId), data);
         return response.data;
-    }
-}; 
+    },
+
+    // Obtener todos los usuarios paginados (orden descendente)
+    getUsersDesc: async (params?: UserPaginationParams): Promise<PaginatedResponse<User>> => {
+        const { data } = await api.get<PaginatedResponse<User>>(
+            API_ENDPOINTS.USERS.GET_ALL,
+            { params }
+        );
+        return data;
+    },
+
+    // Filtrar usuarios por rol
+    getUsersByRole: async (role: Role, params?: UserPaginationParams): Promise<PaginatedResponse<User>> => {
+        const { data } = await api.get<PaginatedResponse<User>>(
+            API_ENDPOINTS.USERS.GET_BY_ROLE(role),
+            { params }
+        );
+        return data;
+    },
+};
