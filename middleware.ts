eeddd1d349491650f0b469d404f8d30014732cd0
Axel <dsg4857@gmail.com>
2026-05-15 06:33:00 -0500

    fix: corregir el nombre de la cookie de autenticación en el middleware

--- middleware.ts
+++ middleware.ts
@@ -3,7 +3,7 @@ import type { NextRequest } from "next/server";
 import { ROUTES } from "@/shared/constants/routes";
 
 export function middleware(request: NextRequest) {
-    const token = request.cookies.get("auth-token")?.value;
+    const token = request.cookies.get("vercel-jwt")?.value;
     const user = { role: request.cookies.get("role")?.value };
     const { pathname } = request.nextUrl;
 
@@ -16,15 +16,19 @@ export function middleware(request: NextRequest) {
 
     const isAuthRoute = pathname.startsWith("/login") || pathname.startsWith("/register")
 
+    // Si es una ruta protegida y el usuario que la visita no posee un token se redirige
     if (isProtectedRoute && !token) {
         const loginUrl = new URL(ROUTES.AUTH.LOGIN, request.url)
         loginUrl.searchParams.set("callbackUrl", pathname)
         return NextResponse.redirect(loginUrl)
     }
 
+    // Si es una ruta de autenticación y el usuario ya tiene un token, 
+    // se redirige a su perfil
     if (isAuthRoute && token) {
         return NextResponse.redirect(new URL(ROUTES.AUTH.MY_PROFILE, request.url))
     }
+
 
     if (pathname.startsWith("/dashboard") && user?.role !== "ADMIN") {
         return new NextResponse("Not Found", { status: 404 });
