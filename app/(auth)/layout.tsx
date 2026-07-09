c9bbeda7ff88e99e739849058d54f33d0167250f
Axel <dsg4857@gmail.com>
2026-07-09 18:48:51 -0500

    refactor: corregir vista responsive para login

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -6,12 +6,16 @@ import FooterComponent from '@/shared/components/ui/home/footer'
 export default function AuthLayout ({ children }: { children: React.ReactNode }) {
     return(
         <div className="flex flex-col min-h-screen">
-            <NavbarServer />
-            <main className="flex-1 pt-20">
+            <header className="hidden md:block">
+                <NavbarServer />
+            </header>
+            <main className="flex-1 md:pt-20">
                 {children}
                 <Toaster/>
             </main>
-            <FooterComponent />
+            <footer className="hidden md:block">
+                <FooterComponent />
+            </footer>
         </div>
     )
 }
